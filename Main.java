9383fc7d21cfe8d9903fca9bb296050a9f8b407b
adr4142 <danielaulloa.000@gmail.com>
2022-07-12 17:09:04 -0300

    Añadidos a Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hola mundo");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("¿Cuál es tu nombre?: ");
+        String nombre = sc.nextLine();
+        System.out.println("Adiós " +nombre+"!");
     }
 }
